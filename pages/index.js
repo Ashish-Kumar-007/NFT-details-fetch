a8287e92b8650037646a9df5a3e704f25abbf30a
Ashish Kumar <91486956+Ashish-Kumar-007@users.noreply.github.com>
2022-11-11 07:37:39 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -28,9 +28,7 @@ export default function Home() {
       }
     };
 
-    console.log('====================================');
-    console.log(process.env.NEXT_PUBLIC_API_KEY);
-    console.log('====================================');
+
     //data -> stores the response data
     let data;
     await axios.request(options)
